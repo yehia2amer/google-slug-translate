46c7782e4ae7680d484c2bf852dfac9aee3ce719
yehia Amer <yehia2amer@yahoo.com>
2019-08-10 15:59:51 +0200

    Translate Chinese

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: Google Slug Translate
 Plugin URI: http://donkey.name/wordpress-plugins/google-slug-translate/
-Description: 使用Google翻译文章别名。需要您在固定链接结构中包含“%postname%”占位符。
+Description: Use Google Translate article alias. You need to include the "%postname%" placeholder in your permalink structure.
 Version: 1.0
 Author: 骑驴觅驴
 Author URI: http://donkey.name/
 */
 
-// 此文件不允许直接访问
+// This file does not allow direct access
 if (!defined('ABSPATH')) {
 	header('HTTP/1.1 403 Forbidden');
 	header('Content-Type: text/plain');
@@ -16,36 +16,34 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * 定义 Google Slug Translate 插件版本
- * 
+ * Define the Google Slug Translate plugin version * 
  * @var string
  */
 define('DONKEY_GST_VERSION', '1.0');
 
 /**
- * 定义 Google Slug Translate 插件路径
- *
+ * Define the Google Slug Translate plugin path *
  * @var string
  */
 define('DONKEY_GST_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
 /**
- * 定义 Google Slug Translate 插件URL
+ * Define the Google Slug Translate plugin URL
  * 
  * @var string
  */
 define('DONKEY_GST_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-// 永久链接结构
+// Permalink structure
 $donkey_permalink = get_option('permalink_structure');
 
-// 仅当处于管理面板，并且永久链接结构中包含“%postname%”和
-// “%pagename%”时才加载 Google Slug Translate 插件
+// only when in the admin panel, and the permalink structure contains "%postname%" and
+// The Google Slug Translate plugin is loaded when "%pagename%"
 if (defined('WP_ADMIN') && WP_ADMIN
 		&& (false !== strpos($donkey_permalink, '%postname%') 
 				|| false !== strpos($donkey_permalink, '%pagename%'))) {
 	require_once(DONKEY_GST_PLUGIN_PATH . 'google-slug-translate.php');
 }
 
-// 取消变量
+// cancel the variable
 unset($donkey_permalink);
